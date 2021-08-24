05864b8280f6f60fdefeb843e43fb89602a70524
ndcnf <84449287+ndcnf@users.noreply.github.com>
2021-08-24 12:17:17 +0200

    Add files via upload

---
+++ C06/test/ex00/ft_print_program_name.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nchennaf <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/20 11:20:04 by nchennaf          #+#    #+#             */
+/*   Updated: 2021/08/21 17:58:18 by nchennaf         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	argc = 0;
+	ft_putstr(argv[argc]);
+	ft_putchar('\n');
+	return (0);
+}
---
+++ C06/test/ex01/ft_print_params.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nchennaf <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/20 11:41:32 by nchennaf          #+#    #+#             */
+/*   Updated: 2021/08/20 14:39:51 by nchennaf         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	y;
+
+	y = 1;
+	while (y < argc)
+	{
+		ft_putstr(argv[y]);
+		ft_putchar('\n');
+		y++;
+	}
+	return (0);
+}
